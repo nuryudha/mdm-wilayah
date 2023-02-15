95815053db69beafc66e4e26d3788b028cc62b74
nuryudha <nur.yudha16@gmail.com>
2023-02-15 10:46:51 +0700

    NEgara

--- src/app/component/wilayah.service.ts
+++ src/app/component/wilayah.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NegaraModel } from '../model/negaraModel';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -8,26 +9,5 @@ import { NegaraModel } from '../model/negaraModel';
 export class WilayahService {
   constructor(private httpClient: HttpClient) {}
 
-  httpOptions: any;
-  url = 'http://localhost:8000/api/foods';
-
-  getDataFoods() {
-    return this.httpClient.get(this.url, this.httpOptions);
-  }
-
-  getViewId(id: NegaraModel) {
-    return this.httpClient.get(this.url + '/' + id);
-  }
-
-  getUpdateData(id: NegaraModel, data: NegaraModel) {
-    return this.httpClient.put(this.url + '/' + id, data);
-  }
-
-  deleteData(id: NegaraModel) {
-    return this.httpClient.delete(this.url + '/' + id);
-  }
-
-  insertData(data: NegaraModel) {
-    return this.httpClient.post(this.url, data);
-  }
+  getCountry() {}
 }
